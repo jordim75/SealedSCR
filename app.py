aa6ec1a2338a73bc5bdce5ab9e329fcf861a49a2
jordim75 <106239022+jordim75@users.noreply.github.com>
2025-12-08 18:13:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,6 @@ cartes_Gothic = { "Ordinary": cartes_dfGothic[cartes_dfGothic["tipus"] == "Ordin
              "Unique": cartes_dfGothic[cartes_dfGothic["tipus"] == "Unique"].to_dict(orient="records"),
             }
 
-cartes_Beta = cartes_beta(cartes_dfBeta)
-cartes_AL = cartes_AL(cartes_dfAL)
-cartes_Gothic = cartes_Gothic(cartes_dfGothic)
 cartes_DL = cartes_dfDL.to_dict(orient="records")
 
 # --- GENERADORS DE SOBRES ---
